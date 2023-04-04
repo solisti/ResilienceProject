b5c171c16ff1cbc3dd9c25a40d581141bbce696b
solisti <89803672+solisti@users.noreply.github.com>
2023-04-04 05:02:43 -0500

    Calculated various slowdowns

--- Step3_solving.m
+++ Step3_solving.m
@@ -13,7 +13,7 @@ A = Problem.A;
 disp('Done loading matrix');
 drawnow('update');
 [N, ~] = size(A);
-result_filename = ['./data/Step3_', matrixname, '_iter=', num2str(bitflip_iter), '.dat'];
+% result_filename = ['./data/Step3_', matrixname, '_iter=', num2str(bitflip_iter), '.dat'];
 rel_grad_filename = ['./matrices/', matrixname, '_gradient_relative.mat'];
 abs_grad_filename = ['./matrices/', matrixname, '_gradient_absolute.mat'];
 
@@ -57,43 +57,64 @@ if exist(new_error, 'file') < 1 % file does not exist
         indices = datasample([1:noerror_converge], M, 'Replace',false); % find the interations we want to inject error in
         E = datasample([1:N], M, 'Replace',false); % error in M random locations from 1 to N
 
+        injections = [2, round(noerror_converge * .25), round(noerror_converge * .5), round(noerror_converge * .75), round(noerror_converge - 1)];
     end
-    save(new_error, 'indices', 'E');
+    save(new_error, 'indices', 'E', 'injections');
 else % file already exists, then just load the file 
-    load(new_error, 'indices', 'E');
+    load(new_error, 'indices', 'E', 'injections');
     M = length(E);
 end
 
+temp_error = ['./matrices/', matrixname, '_temp_error.mat'];
+if exist(temp_error, 'file') < 1
+    error_2 = randi(N, [2, M]);
+    error_5 = randi(N, [5, M]);
+    % E = randi(N, [num_elements, M]);
+    save(temp_error, 'error_2', 'error_5');
+else
+    load(temp_error, 'error_2', 'error_5');
+end 
 
 %% start pcg 
-for m = 1:M
-    % Inject errors from Experiment 1 to M, each at a random location 
-    inject_error = 1;
-    bitflip_pos = E(:, m);
-    bitflip_iter = indices(:,m);
-    % bitflip_iter = 16;
-    
-    load(iter_filename, 'noerror_converge');
-    error_max_iter = noerror_converge*100;
-    
-%         [~,flag,iter,diff_v] = pcg3(A, b, tol, error_max_iter, L, L', inject_error, bitflip_pos, bitflip_iter);
-    [~,flag,iter,diff_v,first_temp_gradient,first_rel_gradient, p] = pcg4(A,b,tol,error_max_iter,L,L', inject_error,bitflip_pos,bitflip_iter);
-    converge = iter;   % number of iterations in error-injecting run
+% for i = injections
+    bitflip_iter = 110;
+    % bitflip_pos = error_2;
+    result_filename = ['./data/Step3_', matrixname, '_iter=', num2str(bitflip_iter), 'error_no = 5', '.dat'];
+    pos_filename = ['./data/Step3_', matrixname, '_iter=', num2str(bitflip_iter), 'error_no = 2, error_pos', '.dat'];
+    for m = 1:M
 
-    grad_abs(:, m) = first_temp_gradient;
-    grad_rel(:, m) = first_rel_gradient;
+        % Inject errors from Experiment 1 to M, each at a random location 
+        inject_error = 1;
+        bitflip_pos = error_5(:, m);
+        % bitflip_iter = indices(:,m);
 
-    % p
 
-    result = [N,flag,bitflip_iter,bitflip_pos,diff_v,A_row_2norm(bitflip_pos),noerror_converge,converge, p(bitflip_pos),grad_abs(bitflip_pos), grad_rel(bitflip_pos)];
-    dlmwrite(result_filename, result, '-append');
+        
+        load(iter_filename, 'noerror_converge');
+        error_max_iter = noerror_converge*100;
+        
+    %         [~,flag,iter,diff_v] = pcg3(A, b, tol, error_max_iter, L, L', inject_error, bitflip_pos, bitflip_iter);
+        [~,flag,iter,diff_v,first_temp_gradient,first_rel_gradient, p] = pcg4(A,b,tol,error_max_iter,L,L', inject_error,bitflip_pos,bitflip_iter);
+        converge = iter;   % number of iterations in error-injecting run
     
-    disp(['Matrix = ', matrixname, ', Experiment=', num2str(m), ', converge=', num2str(converge)]);
-    if flag == 1
-        disp('did not converged');
+        grad_abs(:, m) = first_temp_gradient;
+        grad_rel(:, m) = first_rel_gradient;
+    
+        % p
+    
+        % result = [N,flag,bitflip_iter,bitflip_pos,diff_v,A_row_2norm(bitflip_pos),noerror_converge,converge, p(bitflip_pos),grad_abs(bitflip_pos), grad_rel(bitflip_pos)];
+        result = [N,flag,bitflip_iter,diff_v,noerror_converge,converge]; % for multiple bitflip positions
+        % pos_info = [A_row_2norm(bitflip_pos), p(bitflip_pos), grad_abs(bitflip_pos), grad_rel(bitflip_pos)];
+        dlmwrite(result_filename, result, '-append');
+        % dlmwrite(pos_filename, pos_info, '-append');
+        
+        disp(['Matrix = ', matrixname, ', Experiment=', num2str(m), ', converge=', num2str(converge)]);
+        if flag == 1
+            disp('did not converged');
+        end
     end
         
-end
+% end
 
 
 save(abs_grad_filename, "grad_abs");
